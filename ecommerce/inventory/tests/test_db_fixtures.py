63e3c847c3f5447e5819f63704c534a302ed9486
NasrElsisi <84046654+NasrElsisi@users.noreply.github.com>
2022-10-02 21:28:40 +0200

    start inventory app - test db-category with mark.parametrize

---
+++ ecommerce/inventory/tests/test_db_fixtures.py
@@ -0,0 +1,39 @@
+import pytest
+from ecommerce.inventory.models import Category
+
+
+# # @pytest.mark.dbfixture
+# @pytest.mark.parametrize(
+#     "id, name, slug, is_active",
+#     [
+#         (1, "fashion", "fashion", 1),
+#         # (3, "trainers", "trainers", 1),
+#         # (5, "baseball", "baseball", 1),
+#     ],
+# )
+# def test_inventory_category_dbfixture(db, db_fixture_setup, id, name, slug, is_active):
+
+#     result = Category.objects.get(id=id)
+#     print(result.name)
+#     assert result.name == name
+#     assert result.slug == slug
+#     assert result.is_active == is_active
+
+
+@pytest.mark.parametrize(
+    "name, slug, is_active",
+    [
+        ("fashion", "fashion", 1),
+        ("trainers", "trainers", 1),
+        ("baseball", "baseball", 1),
+    ],
+)
+def test_inventory_category_insert_data(db, db_fixture_setup, name, slug, is_active):
+
+    result = Category.objects.create(
+        name=name, slug=slug, is_active=is_active
+    )
+    print(result.name)
+    assert result.name == name
+    assert result.slug == slug
+    assert result.is_active == is_active
